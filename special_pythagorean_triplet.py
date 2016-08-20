827f3ef9b714d2f53c4fb34cad26a2e0e3d4f673
Hanley Chan <hanleychan@gmail.com>
2016-08-20 14:03:52 -0700

    Added special_pythagorean_triplet.py

---
+++ special_pythagorean_triplet.py
@@ -0,0 +1,36 @@
+"""
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+Find the product abc
+"""
+
+def find_abc_pythagorean(perimeter):
+    """ Returns the first values of a,b and c such that a**2 + b**2 = c**2 
+    given the perimeter value """
+    a, b, c = 1, 1, 1
+
+    while a <= perimeter: # Loop through all possible values of a
+        old_b = b
+
+        while b <= perimeter - a: # Loop through all possible values of b
+            c = perimeter - b - a
+            
+            if a*a + b*b == c*c:
+                return a,b,c 
+            
+
+            if b+1 <= perimeter - a:
+                b+=1     
+            else:
+                break
+
+        a += 1
+        b = old_b 
+        c=1
+    else: # No value found
+        return False
+
+result = find_abc_pythagorean(1000)
+
+if result:
+    print(result[0] * result[1] * result[2])
+

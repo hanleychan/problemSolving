c8254393d9608865ac80a4bad0a46eb7c30a77b3
Hanley Chan <hanleychan@gmail.com>
2016-08-19 22:06:29 -0700

    Added even_fibonacci_numbers.py

---
+++ even_fibonacci_numbers.py
@@ -0,0 +1,28 @@
+"""
+Find the sum of the even valued terms in the Fibonacci sequence that are under four million 
+
+First 10 terms: 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...
+
+"""
+
+def is_even(n):
+    """ Returns whether n is an even number """
+    return n % 2 == 0
+
+def fibonacci(n):
+    """ Find the fibonacci value at position n """
+    if n == 1 or n == 2:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+
+result = 0
+index = 1
+
+while fibonacci(index) < 4000000:
+    if is_even(fibonacci(index)):
+        result += fibonacci(index)
+
+    index += 1
+
+print(result)

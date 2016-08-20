e9cfeed0c5fcb4018cec4a1303bfd83732612ab2
Hanley Chan <hanleychan@gmail.com>
2016-08-19 17:03:14 -0700

    Initial commit

---
+++ multiples_of_3_or_5.py
@@ -0,0 +1,17 @@
+"""
+Finds the sum of all the multiples of 3 or 5 below 1000
+"""
+
+def multiple_of_3_or_5(n):
+    """ Returns whether n is a multiple of 3 or 5 """
+    if n % 3 == 0 or n % 5 == 0:
+        return True
+    return False
+
+result = 0
+for i in range(1, 1000):
+    if multiple_of_3_or_5(i):
+        result += i
+
+print(result)
+

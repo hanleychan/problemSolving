f0e2da1feab238e13cbc977b2690dedb61ef90e1
Hanley Chan <hanleychan@gmail.com>
2016-09-09 17:26:41 -0700

    Added digit_factorials.py

---
+++ projectEuler/digit_factorials.py
@@ -0,0 +1,30 @@
+"""
+Find the sum of all numbers which are equal to the sum of the factorial of their digits
+"""
+
+def factorial(n):
+    """ Returns the value of n! """
+    if n == 0:
+        return 1
+
+    result = 1
+    for i in range(2, n+1):
+        result *= i
+
+    return result
+
+
+if __name__ == "__main__":
+    upper_bound = factorial(9) * 7 # 2540160, 10^(d-1) <= n <= 9!d
+
+    results = []
+
+    for number in range(3, upper_bound):
+        sum_factorials = 0
+        for digit in str(number):
+            sum_factorials += factorial(int(digit))
+
+        if sum_factorials == number:
+            results.append(number)
+
+    print(sum(results))
